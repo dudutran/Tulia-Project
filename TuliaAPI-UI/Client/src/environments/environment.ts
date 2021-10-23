66882aa99e900ad29fffba0bc99c3e6e433497d1
dutran <lovesandlife@gmail.com>
2021-10-22 22:38:53 -0400

    change to production environment

--- TuliaAPI-UI/Client/src/environments/environment.ts
+++ TuliaAPI-UI/Client/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false
 };
-export const baseUrl = 'https://localhost:44365/api/';
+export const baseUrl = 'https://localhost:44326/api/';
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
@@ -13,4 +13,4 @@ export const baseUrl = 'https://localhost:44365/api/';
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
